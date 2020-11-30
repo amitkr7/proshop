57cda98763b75c123176e943172572f00cb20835
Amit <amit840901@gmail.com>
2020-12-01 00:06:37 +0530

    custom error handling

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,18 @@
+const notFound = (req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`)
+  res.status(404)
+  next(error)
+}
+
+const errorHandler = (err, req, res, next) => {
+  //sometimes server respond with 200 status code even if it is an error
+  //to handle such situation
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+  res.status(statusCode)
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+  })
+}
+
+export { notFound, errorHandler }
