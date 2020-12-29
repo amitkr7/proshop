355371409f2b8ebdcb0727d3944f1941ba8052c2
Amit <amit840901@gmail.com>
2020-12-29 18:10:57 +0530

    Product Search Functionality

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,7 +5,19 @@ import Product from '../models/productModel.js'
 //@route    GET /api/products
 //@access   Public
 const getProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find({})
+  //to get query string
+  const keyword = req.query.keyword
+    ? {
+        //this will match keyword with name of the product
+        name: {
+          //regex to avoid exact search and provide search flexibility
+          $regex: req.query.keyword,
+          $options: 'i',
+        },
+      }
+    : {}
+
+  const products = await Product.find({ ...keyword })
   res.json(products)
 })
 
