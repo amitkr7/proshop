5154152ac3359bf10e9a993ef8eec41a6b4fdf35
Amit <amit840901@gmail.com>
2020-12-31 01:10:58 +0530

    Custom page titles and Meta

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
@@ -6,6 +7,7 @@ import Loader from '../components/Loader'
 import Message from '../components/Message'
 import Paginate from '../components/Paginate'
 import ProductCarousel from '../components/ProductCarousel'
+import Meta from '../components/Meta'
 import { listProducts } from '../actions/productActions'
 
 const HomeScreen = ({ match }) => {
@@ -24,8 +26,16 @@ const HomeScreen = ({ match }) => {
 
   return (
     <>
+      <Meta />
+
       {/* do not show carousel on search page */}
-      {!keyword && <ProductCarousel />}
+      {!keyword ? (
+        <ProductCarousel />
+      ) : (
+        <Link to='/' className='btn btn-light'>
+          Go Back
+        </Link>
+      )}
       <h3>Latest Products</h3>
       {loading ? (
         <Loader />
