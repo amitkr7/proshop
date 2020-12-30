d36250043939984b1ac0b7980435d01ccc080f27
Amit <amit840901@gmail.com>
2020-12-30 22:34:47 +0530

    Get topRated Products(backend)

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -155,6 +155,16 @@ const createProductReview = asyncHandler(async (req, res) => {
   }
 })
 
+//@desc   Get top products
+//@route  GET /api/products/top
+//@access Public
+
+const getTopProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({}).sort({ rating: -1 }).limit(3)
+
+  res.json(products)
+})
+
 export {
   getProducts,
   getProductById,
@@ -162,4 +172,5 @@ export {
   createProduct,
   updateProduct,
   createProductReview,
+  getTopProducts,
 }
